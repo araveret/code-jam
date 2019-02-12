f889e2b70ccaeab81dbf8895a489a73be87ca9e0
Adam Raveret <adamraveret@Adams-MacBook-Pro.local>
2019-02-11 23:23:53 -0800

    Adding second solution

--- 2017/Qualification/B_TidyNumbers/TidyNumbers.py
+++ 2017/Qualification/B_TidyNumbers/TidyNumbers.py
@@ -1,16 +1,20 @@
 
 def solver(K, case):   
-    for num in range(len(K)):
-        
-    if '-' in S:
-        print('CASE #{}: IMPOSSIBLE'.format(case))
-    else:
-        print('CASE #{}: {}'.format(case, flips))
-        
+	if len(K)>1:
+	    for x in range(1,len(K)):
+	        if int(K[-x]) < int(K[-x-1]):
+	        	K = list(K)
+	        	K[-x:] = ['9']*len(K[-x:])
+	        	K[-x-1] = str(int(K[-x-1])-1)
+	        	K = ''.join(K)
+	    print('CASE #{}: {}'.format(case, str(int(K))))
+	else:
+		print('CASE #{}: {}'.format(case, str(int(K))))
+
 import fileinput
 f = fileinput.input()    
 T = int(f.readline())
 
 for case in range(1,T+1):
-    K = f.readline()
-    solver(int(K), case)
+    K = f.readline().split()[0]
+    solver(K, case)
